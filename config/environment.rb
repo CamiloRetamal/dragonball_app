8d3ea845b22591b1cf60841ee1ccdaf519360151
CamiloRetamal <camiloretamalmora@gmail.com>
2025-06-22 18:00:40 -0400

    chore: Configurar UTF-8 como codificación por defecto

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,7 @@
+# Establecer la codificación por defecto a UTF-8
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Load the Rails application.
 require_relative "application"
 
